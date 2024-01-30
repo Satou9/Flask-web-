545735cb843f86c03a42b92eb28e10f0e12d9d4c
Satou9 <122270657+Satou9@users.noreply.github.com>
2024-01-30 11:18:23 +0100

    hanae

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
-from flask import Flask
-from view import view
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.register_blueprint(view,url_prefix="/")
+
+PRODUCTS = [
+    {
+        'id': 1,
+        'title': 'Blush',
+        'price': '20',
+
+    },
+    {
+        'id': 2,
+        'title': 'Eyeshadow',
+        'price': '30',
+
+    }, 
+    {
+        'id': 3,
+        'title': 'Highlighter',
+        'price': '40',
+
+    },
+]
+
+#from .view import view
+#from .auth import auth
 
 
-if __name__  == '__main__':
-    app.run(debug=True, port=8000)
+#app.register_blueprint(view, url_prefix='/')
+#app.register_blueprint(auth, url_prefix='/')
+
+ 
+@app.route('/')
+def home():
+    return render_template('index.html', products=PRODUCTS)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
